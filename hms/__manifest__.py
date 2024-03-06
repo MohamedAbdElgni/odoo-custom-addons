03f57ea722b63536303ef7f026b008383923b2f3
MohamedAbdElgni <mohamed.kk878@gmail.com>
2024-03-06 15:58:25 +0200

    patient views done lab1

---
+++ hms/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': "HMS",
+    'author': "Mohamed Abd-ElGani",
+    'category': 'Management',
+    'version': '17.0.0.1.0',
+    'depends': ['base',
+                ],
+    'application': True,
+    'data': [
+        'security/ir.model.access.csv',
+        'views/base_menus.xml',
+        'views/patient.xml',
+
+    ],
+
+}
